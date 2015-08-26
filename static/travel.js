8decb38a2950d64c2850ea9ad1e25116647e08ff
Susan Chin <Susan@Susans-MacBook-Pro.local>
2015-08-25 23:00:08 -0700

    tried to make a prettier homepage but not looking great.

--- static/travel.js
+++ static/travel.js
@@ -29,6 +29,57 @@ google.load('visualization', '1', {packages: ['corechart']});
 google.setOnLoadCallback(initialize);
 
 
+// on document ready, must check for placeholder
+$(document).ready(function(){
+
+	// Test for placeholder support
+	$.support.placeholder = (function(){
+	    var i = document.createElement('input');
+	    return 'placeholder' in i;
+	})();
+
+	// Hide labels by default if placeholders are supported
+	if($.support.placeholder) {
+	    $('.row .form-group label').each(function(){
+	        $(this).addClass('js-hide-label');
+	    });  
+	 
+		$('.row .form-group').find('input').on('keyup blur focus', function(e){
+		     
+		    // Cache our selectors
+		    var $this = $(this),
+		        $parent = $this.parent();
+		 	// console.log("this " + $this.html());
+		 	console.log("parent" + $parent.html());
+		    // Add or remove classes
+		    if (e.type == 'keyup') {                   
+				if( $this.val() == '' ) {
+				    $parent.addClass('js-hide-label'); 
+				    console.log("parent" + $parent.html());
+				} else {
+				    $parent.removeClass('js-hide-label');   
+				}
+		    } 
+		    else if (e.type == 'blur') {
+				if( $this.val() == '' ) {
+				    $parent.addClass('js-hide-label');
+				} 
+				else {
+				    $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
+				}
+		        
+		    } 
+		    else if (e.type == 'focus') {
+				if( $this.val() !== '' ) {
+				    $parent.removeClass('js-unhighlight-label');
+				    console.log("parent " + $parent.html());
+				}
+		    }
+		});
+	} 
+});
+
+
 
 
 // ALL WEBAPP FUNCTIONS START HERE
