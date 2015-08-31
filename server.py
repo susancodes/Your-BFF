63e2326bf937a8e164ebd76074f0995d42af6834
Susan <susancodes@gmail.com>
2015-08-31 14:26:07 -0700

    changed some fonts around

--- server.py
+++ server.py
@@ -308,7 +308,7 @@ def get_weather(lat, lon, date):
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
 
     connect_to_db(app)
 
