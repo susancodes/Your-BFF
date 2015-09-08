ce839c7e3ae1b0d17899f55ee2948be1d1a20982
Susan <susancodes@gmail.com>
2015-09-08 16:35:52 -0700

    use heroku port

--- server.py
+++ server.py
@@ -358,4 +358,9 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     DebugToolbarExtension(app)
 
-    app.run()
+	PORT = int(os.environ.get("PORT", 5000))
+
+	app.run(debug=True, host="0.0.0.0", port=PORT)
+
+
+
