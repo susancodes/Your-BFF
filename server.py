acd6910812331861b9458ab1b4d2d00dace940f5
Susan <susancodes@gmail.com>
2015-09-08 17:14:15 -0700

    heroku deployment

--- server.py
+++ server.py
@@ -353,7 +353,6 @@ def get_instagram():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = True
 
     connect_to_db(app)
 
